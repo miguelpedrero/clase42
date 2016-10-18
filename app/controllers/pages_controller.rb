5c546ad1f3e00c41920104e5b87f7722d2ddfc30
Miguel Pedrero <miguel.servimarket@gmail.com>
2016-10-17 21:28:42 -0300

    formulario

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
-class PagesController < ApplicationController
+class PublicsController < ApplicationController
   def index
-  end
+	@users = User.all
+	end
+
+  def save_user
+  	User.create({ name: params[:name], email: params[:email],age: params[:age]})
+  	redirect_to pages_index_path
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'pages/index'
 
+  get 'pages/index'
+  post 'pages/save_user'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
